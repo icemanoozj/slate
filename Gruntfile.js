90bdc84a3696890b0fb49fb009d23824926a2759
deploy.sh <zhuj02@gmail.com>
2017-05-15 18:17:21 -0700

    add language sel funciton.

---
+++ Gruntfile.js
@@ -0,0 +1,46 @@
+var rf = require("fs");
+
+module.exports = function (grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json')
+    });
+
+    // Default task(s).
+    grunt.registerTask('default', ['']);
+
+    grunt.registerTask('addLanguageSel', 'Add Language selection in index*.html.', function (arg1, arg2) {
+        if (arguments.length === 0) {
+            grunt.log.writeln('Task' + this.name + ", No parameter");
+        }
+        var indexFileNames = ["build/index.html", "build/index_zh_cn.html", "build/index_zh_tw.html"];
+
+        for (var f = 0; f < indexFileNames.length; f++) {
+             console.log(f);
+            var data = rf.readFileSync(indexFileNames[f], "utf-8");
+            var file_postfix = ".html";
+            var template = rf.readFileSync("source/patches/language_sel"+file_postfix, "utf-8");
+            var lines = data.split('\n');
+            var tocifyFound = false;
+            var divCounter = 0;
+            for (var i = 0; i < lines.length; i++) {
+                if (lines[i].includes("tocify-wrapper")) {
+                    console.log(i);
+                    console.log(lines[i]);
+                    tocifyFound = true;
+                }
+                if (tocifyFound && lines[i].includes("</div>")) {
+                    divCounter = divCounter+1;
+                }
+                if (divCounter >= 1) {
+                    lines.splice(i+1,0,template);
+                    break;
+                }
+            }
+            rf.writeFileSync(indexFileNames[f], lines.join("\n"), "utf-8");
+            console.log("READ FILE SYNC END："+ indexFileNames[f]);
+        }
+        
+    });
+};
--- build.bat
+++ build.bat
@@ -1,1 +1,2 @@
 bundle exec middleman build --clean
+grunt addLanguageSel
